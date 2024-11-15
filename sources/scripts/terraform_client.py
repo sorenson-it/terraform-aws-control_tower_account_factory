590895e189606e226ce398d490e6903e7b2a64c2
Guner Brisbin <guner.brisbin@rackspace.com>
2024-11-15 11:07:12 -0600

    missed a input variable for workspace manager script

--- sources/scripts/terraform_client.py
+++ sources/scripts/terraform_client.py
@@ -51,8 +51,8 @@ def check_workspace_exists(organization_name, workspace_name, api_token):
             return data["data"]["id"]
     return None
 
-
-def create_workspace(organization_name, workspace_name, api_token, project_name):
+# def create_workspace(organization_name, workspace_name, api_token, project_name):
+def create_workspace(organization_name, workspace_name, api_token):
     workspace_id = check_workspace_exists(organization_name, workspace_name, api_token)
     project_id = get_project_id(organization_name, project_name, api_token)
     if workspace_id:
